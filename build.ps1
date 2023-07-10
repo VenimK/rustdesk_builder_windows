6264af41de31f95ae38b18696eb0001c2f7bcc28
Wuppie007 <joe@solcon.nl>
2023-07-10 15:55:50 +0200

    Update build.ps1
    Updated to tag 1.2.0

--- build.ps1
+++ build.ps1
@@ -3,18 +3,18 @@ $buildir = 'C:\buildrustdesk'
 
 $CARGO_NDK_VERSION = "3.1.2"
 $LLVM_VERSION= "15.0.6"
-$FLUTTER_VERSION= "3.10.1"
+$FLUTTER_VERSION= "3.10.5"
 $FLUTTER_RUST_BRIDGE_VERSION= "1.75.3"
 
+# Custom settings, re-open your window after setting the variables or run the script twice to build rustdesk with custom settings
 # [System.Environment]::SetEnvironmentVariable(‘RS_PUB_KEY’,'<<key>>',"Machine");
 # [System.Environment]::SetEnvironmentVariable(‘RENDEZVOUS_SERVER’,'<<yourownserver>>',"Machine");
-
 
 cd $buildir
 echo "Checkout code"
 git clone https://github.com/rustdesk/rustdesk.git --quiet
 cd rustdesk
-git reset --hard c90b2db 
+git reset --hard $buildcommit 
 
 New-Item -ItemType SymbolicLink -Path (Join-Path ($buildir)('rustdesk\res\icon.ico')) -Target (Join-Path ($buildir)('rustdesk\flutter/windows/runner/resources/app_icon.ico')) -Force
 
