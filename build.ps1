809b06682d7ced0b44257bcdb1e2bfbaa8fed9e1
VenimK <venimkmac@gmail.com>
2024-06-11 05:24:09 +0200

    Update build.ps1 - Changed Version Flutter to latest

--- build.ps1
+++ build.ps1
@@ -3,8 +3,8 @@ $buildir = 'C:\buildrustdesk'
 
 $CARGO_NDK_VERSION = "3.1.2"
 $LLVM_VERSION= "15.0.6"
-$FLUTTER_VERSION= "3.10.5"
-$FLUTTER_RUST_BRIDGE_VERSION= "1.75.3"
+$FLUTTER_VERSION= "3.19.5"
+$FLUTTER_RUST_BRIDGE_VERSION= "1.80.1"
 
 # Custom settings, re-open your window after setting the variables or run the script twice to build rustdesk with custom settings
 # [System.Environment]::SetEnvironmentVariable(‘RS_PUB_KEY’,'<<key>>',"Machine");
@@ -18,7 +18,7 @@ git reset --hard $buildcommit
 
 New-Item -ItemType SymbolicLink -Path (Join-Path ($buildir)('rustdesk\res\icon.ico')) -Target (Join-Path ($buildir)('rustdesk\flutter/windows/runner/resources/app_icon.ico')) -Force
 
-Start-Process cargo.exe -ArgumentList 'install flutter_rust_bridge_codegen --version 1.75.3' -Wait
+Start-Process cargo.exe -ArgumentList 'install flutter_rust_bridge_codegen --version 1.80.1' -Wait
 cd flutter
 Start-Process flutter -ArgumentList 'pub get' -Wait
 cd ../
