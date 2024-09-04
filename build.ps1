f858465825a260f0397cbb5c79d4d0a9d3f3c2cd
VenimK <venimkmac@gmail.com>
2024-09-04 08:53:56 +0200

    Update build.ps1 - ADD sciter.dll - Move and adjust src/main.rs

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 $download = 'C:\download'
 $buildir = 'C:\buildrustdesk'
+$sciterUrl = 'https://raw.githubusercontent.com/c-smile/sciter-sdk/master/bin.win/x64/sciter.dll' # Provide the URL where sciter.dll can be downloaded here
 
 $CARGO_NDK_VERSION = "3.1.2"
 $LLVM_VERSION= "15.0.6"
@@ -20,6 +21,14 @@ cd rustdesk
 git reset --hard $buildcommit 
 #################################################################
 
+# Download sciter.dll
+Write-Host "Downloading sciter.dll"
+Invoke-WebRequest -Uri $sciterUrl -OutFile (Join-Path $download 'sciter.dll')
+
+# Move sciter.dll to the source directory
+Write-Host "Moving sciter.dll to the rustdesk source map"
+Move-Item -Path (Join-Path $download 'sciter.dll') -Destination (Join-Path $buildir 'rustdesk')
+
 New-Item -ItemType SymbolicLink -Path (Join-Path ($buildir)('rustdesk\res\icon.ico')) -Target (Join-Path ($buildir)('rustdesk\flutter/windows/runner/resources/app_icon.ico')) -Force
 
 Start-Process cargo.exe -ArgumentList 'install flutter_rust_bridge_codegen --version 1.80.1' -Wait
@@ -28,6 +37,14 @@ Start-Process flutter -ArgumentList 'pub get' -Wait
 cd ../
 
 flutter_rust_bridge_codegen --rust-input ./src/flutter_ffi.rs --dart-output ./flutter/lib/generated_bridge.dart
+
+# Update src/main.rs
+Write-Host "Edit and adjust the src/main.rs to load the sciter.dll correctly"
+$mainRSPath = Join-Path $buildir 'rustdesk/src/main.rs'
+
+$mainRSContent = Get-Content $mainRSPath -Raw
+$updatedMainRSContent = $mainRSContent -replace 'let bytes = std::include_bytes\("..\\sciter.dll"\);', 'let bytes = std::include_bytes!("../sciter.dll");'
+Set-Content -Path $mainRSPath -Value $updatedMainRSContent
 
 cd (Join-Path ($buildir)('rustdesk'))
 Remove-Item –path flutter\build –recurse
